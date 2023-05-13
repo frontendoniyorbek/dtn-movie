d27c987e604aaa1fc118942c12cc555bf2c6f545
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-05-13 13:47:22 +0500

    index.ts export Row

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as Header } from './header/Header';
-export { default as Row } from './row/row';
+export {default as Row } from './row/Row'
 export { default as Hero } from './hero/hero';
 export { default as TextField } from './text-field/text-field';
 export { default as Modal } from './modal/modal';
